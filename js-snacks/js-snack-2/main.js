505419c4a6587d7862f8e91b31b77d37215da0e6
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-30 11:36:02 +0200

    Fix typo in word length comparison logic and improve alert messages in js-snack-2

--- js-snacks/js-snack-2/main.js
+++ js-snacks/js-snack-2/main.js
@@ -6,15 +6,15 @@ const secondWord = prompt('Scrivi una seconda parola')
 
 //  Il software stampa prima la parola più corta poi la parola più lunga
 if (firstWord.length > secondWord.lentgh) {
-    alert('La parola più lunga è : ' + firstWord)
     alert('La parola più corta è : ' + secondWord)
-    console.log('La parola più lunga è : ' + firstWord)
+    alert('La parola più lunga è : ' + firstWord)
     console.log('La parola più corta è : ' + secondWord)
+    console.log('La parola più lunga è : ' + firstWord)
 } else if (firstWord < secondWord) {
-    alert('La parola più lunga è : ' + secondWord)
     alert('La parola più corta è : ' + firstWord)
-    console.log('La parola più lunga è : ' + secondWord)
+    alert('La parola più lunga è : ' + secondWord)
     console.log('La parola più corta è : ' + firstWord)
+    console.log('La parola più lunga è : ' + secondWord)
 } else {
     alert('Le parole sono della stessa lunghezza')
     console.log('Le parole sono della stessa lunghezza')
