962790197ed5f14440cdc90afba53944bd60a99e
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-30 12:39:32 +0200

    Refactor loop structure for user input in odd number collection

--- js-snacks/js-snack-4/main.js
+++ js-snacks/js-snack-4/main.js
@@ -5,7 +5,7 @@
 const oddNumbers = [];
 let repeat = 6;
 // Chiedi per 6 volte all'utente di inserire un numero,
-
+// let i = 0; i < repeat; i++
 for (let i = repeat; i > 0; i--) {
     let singleNumber = Number(prompt('Scrivi un numero'))
     if (singleNumber % 2 == 1) {
