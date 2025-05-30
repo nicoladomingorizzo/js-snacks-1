b7dde3c71699af41abeb291b36e94c9dd614efdf
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-30 13:12:18 +0200

    Add HTML and JavaScript files to display powers of 2 up to 1000 in js-bonus-2

---
+++ js-snacks/js-bonus-2/main.js
@@ -0,0 +1,8 @@
+/* Stampa le potenze di 2 fino a 1000 */
+let powNumber = null
+let repeat = 1000
+// let singleNumber = null
+for (let i = 0; i <= repeat; i++) {
+    powNumber = 2 ** i
+    console.log(powNumber)
+}
